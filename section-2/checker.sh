fac07cb5786a4c10798c5681a06ba28f05adb2eb
DELLIN11 <tigran.nikiyan@gmail.com>
2026-05-08 17:14:12 +0300

    Final

--- section-2/checker.sh
+++ section-2/checker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 urls=(
   "http://httpbin.org/status/200"
   "http://httpbin.org/status/301"
@@ -7,12 +6,10 @@ urls=(
   "http://httpbin.org/status/500"
   "http://httpbin.org/status/102"
 )
-
 for url in "${urls[@]}"; do
   response=$(curl -s -w "\n%{http_code}" "$url")
   http_code=$(echo "$response" | tail -n 1)
   body=$(echo "$response" | sed '$d')
-
   if [[ $http_code =~ ^[1-3] ]]; then
     echo "URL: $url"
     echo "Status: $http_code"
@@ -20,10 +17,11 @@ for url in "${urls[@]}"; do
     echo "-------------------------"
   elif [[ $http_code =~ ^[45] ]]; then
     echo "EXCEPTION: Received $http_code from $url"
+    exit 1
   else
     echo "Unexpected status code: $http_code"
+    exit 1
   fi
 done
-
-echo "All responses processed."
+echo "All successful responses processed."
 exit 0
